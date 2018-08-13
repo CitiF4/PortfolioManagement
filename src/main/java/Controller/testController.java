c4f829560d6db1f159e9939d2f1784a1788b7b9b
evan1984 <459018079@qq.com>
2018-08-13 20:57:40 +0800

    add test informationInsert and informationQuery

--- src/main/java/Controller/testController.java
+++ src/main/java/Controller/testController.java
@@ -1,10 +1,12 @@
 package Controller;
 
 import Model.Fundmanager;
+import Model.Information;
 import Model.Portfolio;
 import Service.AdminService;
 import Service.AdminServiceImpl;
 import Service.FundManagerService;
+import Service.InformationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -14,7 +16,9 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -29,11 +33,32 @@ public class testController {
     AdminService adminServiceImpl;
 
     @Autowired
+    @Qualifier("InformationServiceImpl")
+    InformationService informationServiceImpl;
+
+    @Autowired
     @Qualifier("fundmanagerServiceImpl")
     FundManagerService fundmanagerServiceImpl;
 
     @RequestMapping(value = "/test")
     public ModelAndView test(){
+
+        Information info = new Information();
+        info.setCcy("CNY");
+        info.setDate("2018-08-13 20:00:22.721");
+        info.setPrice(100.00);
+        info.setType("futures");
+        info.setSymbol("F4");
+
+        informationServiceImpl.createInformation(info);
+
+        List<Information> result = informationServiceImpl.queryForInformation("CNY");
+        for (Information i :result
+             ) {
+            System.out.println(i.getDate());
+        }
+
+
         Fundmanager f = adminServiceImpl.getFundManager(9999);
         System.out.println("==========================================");
         ModelAndView modelAndView = new ModelAndView();
