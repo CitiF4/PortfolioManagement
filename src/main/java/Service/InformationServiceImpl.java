945b81967faf21bad9ae91747d59f47e6e34c9de
evan1984 <459018079@qq.com>
2018-08-13 21:04:22 +0800

    。。

---
+++ src/main/java/Service/InformationServiceImpl.java
@@ -0,0 +1,26 @@
+package Service;
+import java.util.Date;
+import Dao.*;
+import Model.*;
+import Model.Position;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public class InformationServiceImpl implements InformationService {
+    @Autowired
+    private InformationMapper informationMapper;
+
+    public void createInformation(Information info){
+        informationMapper.createInfomation(info);
+    }
+
+   /* public List<Information> queryForInformation(String type, String date){
+        return informationMapper.queryForInformation(type,date);
+    };*/
+
+    public List<Information> queryForInformation(String type){
+        return informationMapper.queryForInformation(type);
+    };
+}
